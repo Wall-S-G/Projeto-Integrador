209a615399a9b87002200b47902451aa7be87a14
Wallysson Soares Guimarães <202511900027@ifto.local>
2026-06-02 12:29:33 -0300

    bah

--- POO/M/carro.py
+++ POO/M/carro.py
@@ -18,16 +18,57 @@ class Carro:
         self.ano = ano
         self.velocidade = velocidade
 
+    #Métodos:
+
+    #Método Acelerar:
+    #"aumento" será o valor recebido para aumentar a velocidade.
     def acelerar(self, aumento):
-        self.velocidade += self.velocidade + aumento
+        # self.velocidade = self.velocidade + aumento
+        self.velocidade += aumento
 
-Fastback = Carro("Ford", "Mustang", "Fastback", 1969, velocidade=0)
+    def frear(self, reducao):
+        # self.velocidade = self.velocidade - reducao
+        self.velocidade -= reducao
+    
+    def exibir_info(self, escolha):
+        escolha = escolha.lower()
+        if escolha == "marca":
+            print(f"Marca: {self.marca}")
+        elif escolha == "modelo":
+            print(f"Modelo: {self.modelo}")
+        elif escolha == "ano":
+            print(f"Ano: {self.ano}")
+        elif escolha == "nome":
+            print(f"Nome: {self.nome}")
+        elif escolha == "velocidade":
+            print(f"Velocidade: {self.velocidade}km/h")
+        elif escolha == "todas":
+            print(f"marca: {self.marca}, modelo: {self.modelo}, ano: {self.ano}, nome: {self.nome}")
+        else:
+            print("Opção inválida!")
+
+carro1 = Carro("Ford", "Mustang", "Fastback", 1969, velocidade=0)
+
 # Exibir informações do carro
-print(f"marca: {Fastback.marca}     |modelo: {Fastback.modelo}   |ano: {Fastback.ano}     |nome: {Fastback.nome}")
-Fastback.acelerar(50)
-print(f"{Fastback.nome} acelera! Velocidade atual={Fastback.velocidade}km/h.\n")
+print(f"\nmarca: {carro1.marca}     |modelo: {carro1.modelo}   |ano: {carro1.ano}     |nome: {carro1.nome}")
 
-e30 = Carro("BMW", "m3", "e30", 1980, velocidade=0)
-print(f"marca: {e30.marca}      |modelo: {e30.modelo}        |ano: {e30.ano}     |nome: {e30.nome}")
-e30.acelerar(150)
-print(f"{e30.nome} acelera! Velocidade atual={Fastback.velocidade}km/h.\n")
+carro1.acelerar(int(input("Acelerar: ")))
+print(f"{carro1.nome} acelera!\nVelocidade atual:{carro1.velocidade}km/h.")
+
+carro1.frear(int(input("\nFreiar: ")))
+print(f"{carro1.nome} freia!\nVelocidade atual:{carro1.velocidade}km/h.")
+
+carro1.exibir_info(input("\nExibir quais informações?\nmarca, modelo, ano, nome, velocidade ou todas?\n"))
+
+
+carro2 = Carro("BMW", "m3", "e30", 1980, velocidade=0)
+
+print(f"\nmarca: {carro2.marca}      |modelo: {carro2.modelo}        |ano: {carro2.ano}     |nome: {carro2.nome}")
+
+carro2.acelerar(int(input("Acelerar: ")))
+print(f"{carro2.nome} acelera!\nVelocidade atual:{carro2.velocidade}km/h.")
+
+carro2.frear(int(input("\nFreiar: ")))
+print(f"{carro2.nome} freia!\nVelocidade atual:{carro2.velocidade}km/h.")
+
+carro2.exibir_info(input("\nExibir quais informações?\nmarca, modelo, ano, nome, velocidade ou todas?\n"))
---
+++ TopVendas-Comércio/Comércio.py
@@ -0,0 +1,2 @@
+class Produto:
+    def __init__()
