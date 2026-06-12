cfdce5bc7f7e32b10858f665613ba223c88009dc
Wallysson Soares Guimarães <202511900027@ifto.local>
2026-06-12 12:05:09 -0300

    .

--- POO/M/carro.py
+++ POO/M/carro.py
@@ -9,26 +9,28 @@ class Carro:
 
     # "self" representa o próprio objeto.
 
-    # "marca", "modelo", "ano", e "velocidade".
+    # "marca", "modelo", "ano", "velocidade", etc.
 
-    def __init__(self, marca, modelo, nome, ano, velocidade=0):
+    def __init__(self, marca, modelo, nome, ano, consumo=0.0, velocidade=0, velocidade_max=0):
         self.marca = marca
         self.modelo = modelo
         self.nome = nome
         self.ano = ano
+        self.consumo = consumo
         self.velocidade = velocidade
+        self.velocidade_max = velocidade_max
 
-    #Métodos:
-
-    #Método Acelerar:
-    #"aumento" será o valor recebido para aumentar a velocidade.
     def acelerar(self, aumento):
-        # self.velocidade = self.velocidade + aumento
-        self.velocidade += aumento
+        if self.velocidade + acelera > velocidade_max:
+            self.velocidade = velocidade_max
+        else:
+            self.velocidade += aumento
 
     def frear(self, reducao):
-        # self.velocidade = self.velocidade - reducao
-        self.velocidade -= reducao
+        if self.velocidade - reducao < 0:
+            self.velocidade = 0
+        else:
+            self.velocidade -= reducao
     
     def exibir_info(self, escolha):
         escolha = escolha.lower()
@@ -36,20 +38,24 @@ class Carro:
             print(f"Marca: {self.marca}")
         elif escolha == "modelo":
             print(f"Modelo: {self.modelo}")
-        elif escolha == "ano":
-            print(f"Ano: {self.ano}")
         elif escolha == "nome":
             print(f"Nome: {self.nome}")
+        elif escolha == "ano":
+            print(f"Ano: {self.ano}")
+        elif escolha == "consumo=0.0":
+            print(f"Consumo: {self.consumo}")
         elif escolha == "velocidade":
             print(f"Velocidade: {self.velocidade}km/h")
+        elif escolha == "velocidade_max":
+            print(f"Velocidade máxima: {self.velocidade_max}")
         elif escolha == "todas":
-            print(f"marca: {self.marca}, modelo: {self.modelo}, ano: {self.ano}, nome: {self.nome}")
+            print(f"Marca: {self.marca}, modelo: {self.modelo}, ano: {self.ano}, nome: {self.nome}, consumo: {self.consumo}, velocidade máxima: {self.velocidade_max}")
         else:
             print("Opção inválida!")
 
-carro1 = Carro("Ford", "Mustang", "Fastback", 1969, velocidade=0)
+carro1 = Carro("Ford", "Mustang", "Fastback", 1969, consumo=5.8, velocidade=0, velocidade_max=210)
 
-# Exibir informações do carro
+# Exibir informações principais do carro
 print(f"\nmarca: {carro1.marca}     |modelo: {carro1.modelo}   |ano: {carro1.ano}     |nome: {carro1.nome}")
 
 carro1.acelerar(int(input("Acelerar: ")))
@@ -58,10 +64,10 @@ print(f"{carro1.nome} acelera!\nVelocidade atual:{carro1.velocidade}km/h.")
 carro1.frear(int(input("\nFreiar: ")))
 print(f"{carro1.nome} freia!\nVelocidade atual:{carro1.velocidade}km/h.")
 
-carro1.exibir_info(input("\nExibir quais informações?\nmarca, modelo, ano, nome, velocidade ou todas?\n"))
+carro1.exibir_info(input("\nExibir quais informações?\nmarca, modelo, ano, nome, consumo, velocidade, velocidade máxima ou todas?\n"))
 
 
-carro2 = Carro("BMW", "m3", "e30", 1980, velocidade=0)
+carro2 = Carro("BMW", "m3", "e30", 1986, consumo=2.3, velocidade=0, velocidade_max=230)
 
 print(f"\nmarca: {carro2.marca}      |modelo: {carro2.modelo}        |ano: {carro2.ano}     |nome: {carro2.nome}")
 
@@ -71,4 +77,4 @@ print(f"{carro2.nome} acelera!\nVelocidade atual:{carro2.velocidade}km/h.")
 carro2.frear(int(input("\nFreiar: ")))
 print(f"{carro2.nome} freia!\nVelocidade atual:{carro2.velocidade}km/h.")
 
-carro2.exibir_info(input("\nExibir quais informações?\nmarca, modelo, ano, nome, velocidade ou todas?\n"))
+carro2.exibir_info(input("\nExibir quais informações?\nmarca, modelo, ano, nome, consumo, velocidade, velocidade máxima ou todas?\n"))
